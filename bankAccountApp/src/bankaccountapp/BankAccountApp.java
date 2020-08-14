850f4c6c6f81a42afe1f922dbf34d0e70f62b90d
Augs Machado <augustomachadoramos@gmail.com>
2020-08-14 12:07:44 -0300

    Add start account method

--- bankAccountApp/src/bankaccountapp/BankAccountApp.java
+++ bankAccountApp/src/bankaccountapp/BankAccountApp.java
@@ -6,18 +6,24 @@ import javax.swing.JOptionPane;
 public class BankAccountApp {
 
     public static void main(String[] args) {
-        String nome;
-        Random numero = new Random();
+        String name;
+        Random number = new Random();
         
-        // Criando dados do usuário e da conta
-        nome = JOptionPane.showInputDialog("Qual o seu nome completo?");
-        int conta = 1 + numero.nextInt(9999);
-        int senha = 1 + numero.nextInt(9999);
+        // Criando dados do usuário e da account
+        name = JOptionPane.showInputDialog("What's your fullname?");
+        int account = 1000 + number.nextInt(8999);
+        int password = 1000 + number.nextInt(8999);
+        float initialBalance;
+        initialBalance = (float) (1 + number.nextInt(9999));
         
-        // Mostrando dados do usuário e da conta
-        JOptionPane.showMessageDialog(null, "\nNome: " + nome 
-                +"\nNúmero da conta: " +conta
-                +"\nSenha: " +senha);
+        // Mostrando dados do usuário e da account
+        JOptionPane.showMessageDialog(null, "\nFullname: " + name 
+                +"\nAccount number: " +account
+                +"\nPassword: " +password);
+        
+        // Criando a conta de um cliente
+        atmMachine myAccount;
+        myAccount = new atmMachine(name,account,password,initialBalance);
+        myAccount.start();
     }
-    
 }
--- bankAccountApp/src/bankaccountapp/atmMachine.java
+++ bankAccountApp/src/bankaccountapp/atmMachine.java
@@ -1,9 +1,29 @@
-/*
-
-*/
-
 package bankaccountapp;
 
+import javax.swing.JOptionPane;
+
 public class atmMachine {
+    private String name;
+    private int account, password, withdraws;
+    private float balance;
+   
+    public atmMachine(String name, int account, int password, float initialBalance) {
+        this.name = name;
+        this.account = account;
+        this.password = password;
+        balance = initialBalance;
+        withdraws = 0;
+        
+        System.out.print(this.name+"\n"+this.account+"\n"+this.password+"\n"+balance);
+    }
     
+    public static void start(){
+        int option;
+        
+        do {
+            //exibeMenu();
+            option = Integer.parseInt(JOptionPane.showInputDialog("Do you want to acess your account?\n1-Yes\n0-No").trim());
+            //escolherOpcao(opcao);
+        } while(option != 0);
+    }
 }
