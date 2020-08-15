a3a34dec1fe6a971fa2dcc5208678667485e3b17
Augs Machado <augustomachadoramos@gmail.com>
2020-08-15 00:32:18 -0300

    Add Account and Password validation method

--- bankAccountApp/src/bankaccountapp/atmMachine.java
+++ bankAccountApp/src/bankaccountapp/atmMachine.java
@@ -3,27 +3,65 @@ package bankaccountapp;
 import javax.swing.JOptionPane;
 
 public class atmMachine {
-    private String name;
-    private int account, password, withdraws;
+    private final String name;
+    private static int account, password;
+    private int withdraws;
     private float balance;
+    
+    private static final int TRUE = 1, FALSE = 0;
    
     public atmMachine(String name, int account, int password, float initialBalance) {
         this.name = name;
-        this.account = account;
-        this.password = password;
+        atmMachine.account = account;
+        atmMachine.password = password;
         balance = initialBalance;
         withdraws = 0;
-        
-        System.out.print(this.name+"\n"+this.account+"\n"+this.password+"\n"+balance);
     }
     
     public static void start(){
         int option;
-        
+      
         do {
-            //exibeMenu();
-            option = Integer.parseInt(JOptionPane.showInputDialog("Do you want to acess your account?\n1-Yes\n0-No").trim());
-            //escolherOpcao(opcao);
+            option = Integer.parseInt(JOptionPane.showInputDialog("Do you want to access your account?"
+                    + "\n1- Yes"
+                    + "\n0- No").trim());
+            
+            // If the client wants to access the account
+            if(option == TRUE) {
+                option = isValid(account, password);
+                
+                if(option == TRUE) {
+                    JOptionPane.showMessageDialog(null, "Congrats");
+                    showMenu();
+                }
+            }
+            
         } while(option != 0);
+    }
+    
+    // Account and password validation
+    public static int isValid(int account, int password) {
+        int confirmAccount, confirmPassword;
+        
+        for(int i = 0; i <3; i++) {
+            confirmAccount = Integer.parseInt(JOptionPane.showInputDialog("Enter your account"));
+            if(confirmAccount == account) {
+                confirmPassword = Integer.parseInt(JOptionPane.showInputDialog("Enter your password"));
+
+                // When account and password are same, return true
+                if (confirmPassword == password) {
+                    return TRUE;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Account or password are wrong :/");
+        }
+        
+        JOptionPane.showMessageDialog(null, "You lost the access for this account."
+                + "\nTalk with your Bank Manager!");
+        return FALSE;
+    }
+    
+    public static void showMenu() {
+        
     }
 }
